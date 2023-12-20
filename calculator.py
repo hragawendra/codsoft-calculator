2b9370dbb7dcb4e0fd821fab03bf607fe764724c
H.RAGAWENDRA REDDY <152480406+hragawendra@users.noreply.github.com>
2023-12-20 13:09:13 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,53 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        raise ValueError("Error: Division by zero is undefined.")
+
+print("Select operation:")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            try:
+                print(num1, "/", num2, "=", divide(num1, num2))
+            except ValueError as e:
+                print(e)
+                continue
+
+        next_calculation = input("Let's do the next calculation? (y/n): ")
+        if next_calculation == "n":
+            break
+    else:
+        print("Invalid input")
